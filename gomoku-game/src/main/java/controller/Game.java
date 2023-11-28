0c736fc646025718abc01c6e80189606fa30c47f
Gulzhasm <gulzhasm@gmail.com>
2023-11-28 15:28:05 +0600

    basic CLI game

--- gomoku-game/src/main/java/controller/Game.java
+++ gomoku-game/src/main/java/controller/Game.java
@@ -1,16 +1,76 @@
 package controller;
 
-import models.Computer;
-import models.Human;
-import models.Stone;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Game {
-private final int SIZE = 15;
-private Human humanPlayer;
-private Computer computerPlayer;
-private final char[][] board = new char[SIZE][SIZE];
-private List<Stone> stones = new ArrayList<Stone>();
+
+    private static final int SIZE = 15;
+    private static final char HUMAN = 'W';
+    private static final char COMPUTER = 'B';
+    private static final char[][] board = new char[SIZE][SIZE];
+    private static boolean isGameOver = false;
+    private static int humanMoveCount = 0;
+
+    public static void main(String[] args) {
+        initializeBoard();
+        char currentPlayer = HUMAN;
+        while (!isGameOver) {
+            if (currentPlayer == HUMAN) {
+                humanMove();
+            } else {
+                computerMove();
+            }
+
+            if (humanMoveCount >= 5) {
+                if (isGameOver) {
+                    System.out.println("The player " + currentPlayer + " has won!");
+                }
+            }
+
+            currentPlayer = (currentPlayer == HUMAN) ? COMPUTER : HUMAN;
+            displayBoard();
+        }
+    }
+
+    static void computerMove() {
+    }
+
+    static void humanMove() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please Enter indexes: ");
+        int row = scanner.nextInt();
+        int col = scanner.nextInt();
+        if (board[row][col] == ' ') {
+            board[row][col] = HUMAN;
+            humanMoveCount++;
+        } else {
+            System.out.println("Invalid indexes entered!");
+        }
+        if (humanMoveCount >= 5) {
+            isGameOver = checkWin(HUMAN);
+        }
+    }
+
+    static boolean checkWin(char currentPlayer) {
+        return false;
+    }
+
+    static void initializeBoard() {
+        for (char[] chars : board) {
+            Arrays.fill(chars, ' ');
+        }
+    }
+
+    public static void displayBoard() {
+        for (char[] chars : board) {
+            for (char aChar : chars) {
+                System.out.print(aChar + "|");
+            }
+            System.out.println();
+        }
+        System.out.println("-------------------------------");
+    }
+
+
 }
